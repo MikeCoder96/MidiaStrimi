9ee895c8b1d3b2c5360a842fcbb2e35e00c7824b
Mikele <bncmhl96r23a773d@studenti.unical.it>
2020-11-05 22:56:41 +0100

    Updated cli main

--- midiastrimi_cli/Program.cs
+++ midiastrimi_cli/Program.cs
@@ -9,12 +9,33 @@ namespace midiastrimi_cli
     {
         static MainClass mainClass = new MainClass();
         static void Main()
-        {    
-            List<Movie> res = mainClass.getMovieWithTitle(Console.ReadLine(), 2);
-            foreach (var x in res)
+        {
+            Console.WriteLine("Hi, i'm MidiaStrimi CLI and i'm here to help you!\n" +
+                "Before start, please chose a provider:\n1) CB01\n2) AltaDefinizione\n3) YesMovies\n");
+            Console.Write("Your input: ");
+            int provider = int.Parse(Console.ReadLine());
+            Console.Write("Please insert a movie title: ");
+            string movieTitle = Console.ReadLine();
+            List<Movie> retrieved = mainClass.getMovieWithTitle(movieTitle, provider);
+
+            foreach (var x in retrieved)
             {
-                Console.WriteLine(x.getMovieTitle());
-                Console.WriteLine(x.getMovieDesc());
+                Console.WriteLine("Title: " + x.getMovieTitle());
+                int charCounter = 0;
+                Console.Write('\t');
+                string to_print = x.getMovieDesc().Replace("\r", "").Replace("\n", "").Replace("\t", "").Replace("                                                                                               ", " ");
+                for (int i = 0; i < to_print.Length; i++)
+                {
+                    Console.Write(to_print[i]);
+                    charCounter++;
+                    if (charCounter == 97)
+                    {
+                        Console.WriteLine();
+                        charCounter = 0;
+                        Console.Write('\t');
+                    }
+                }
+                Console.WriteLine();
             }
         }
     }
